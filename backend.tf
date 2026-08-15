bbd35bd64f66daaa359dd266126ef69be9771b37
varukoluaravind22 <varukoluaravind22@gmail.com>
2026-08-15 14:35:05 +0530

    ddd

--- backend.tf
+++ backend.tf
@@ -1,19 +1,20 @@
-resource "aws_dynamodb_table" "terraform-lock" {
-  name = "use_lockfile"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+# resource "aws_dynamodb_table" "terraform-lock" {
+#   name = "terraform-lock"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
 terraform {
     backend "s3" {
       bucket = "appchunk"
       key = "Entire_arch/terraform.tfstate"
       region = "ap-south-1"
       encrypt = true
-      dynamodb_table = "use_lockfile"
+      #dynamodb_table = "terraform-lock"
+      use_lockfile = true
     }
 }
 
