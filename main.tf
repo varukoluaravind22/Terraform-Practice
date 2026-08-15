d85b06c0abf17d5c200344862edbfa26db23f33c
varukoluaravind22 <varukoluaravind22@gmail.com>
2026-08-15 18:45:17 +0530

    added dns enable property to vpc

--- main.tf
+++ main.tf
@@ -15,6 +15,9 @@ provider "aws" {
 }
 resource "aws_vpc" "Entire_arch" {
    cidr_block = var.vpc_cidr
+   enable_dns_support   = true
+   enable_dns_hostnames = true
+
    tags = {
      Name = var.vpc_name
    }
