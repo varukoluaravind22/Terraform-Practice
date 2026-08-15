ece8618d149802fe5a9b53bd8be0916c5ae50fce
varukoluaravind22 <varukoluaravind22@gmail.com>
2026-08-15 14:48:19 +0530

    vpc_config is not required in node goup configuration directly subnet_ids = []

--- main.tf
+++ main.tf
@@ -224,22 +224,20 @@ resource "aws_eks_node_group" "Entire_arch_eks_cluster_node_group"{
   cluster_name = aws_eks_cluster.Entire_arch_eks_cluster.name
   node_group_name = "${var.vpc_name}_eks_node_group"
   node_role_arn = aws_iam_role.Entire_arch_node_role.arn
-  vpc_config {
-    subnet_ids =[
-      aws_subnet.Entire_pvt_subnet-1.id,
-      aws_subnet.Entire_pvt_subnet-2.id
-    ]
-  }
-  sacling_config {
+  subnet_ids =[
+    aws_subnet.Entire_pvt_subnet-1.id,
+    aws_subnet.Entire_pvt_subnet-2.id
+  ]
+  scaling_config {
     desired_size = 2
     max_size = 5
     min_size = 1
   }
   update_config{
-    max_unavilable = 1 
+    max_unavailable = 1 
   }
   depends_on = [
-    aws_iam_policy_attachment.Entire-arch_eks_node_policy
+    aws_iam_policy_attachment.Entire-arch_eks_node_policy.arn
   ]
 }
 #node  group  role creation for the eks cluster
